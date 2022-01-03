3a19ccde6e201017bbbac73f4017ac610a2f6546
Pearl Molefe <PMolefe@bec.co.bw>
2022-01-03 19:11:13 +0200

    change lifecycle version + fix about fragment

--- app/src/main/java/com/example/spotted/ui/about/AboutFragment.java
+++ app/src/main/java/com/example/spotted/ui/about/AboutFragment.java
@@ -9,8 +9,11 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.spotted.R;
+import com.example.spotted.ui.home.HomeViewModel;
 
 public class AboutFragment extends Fragment {
 
@@ -20,8 +23,11 @@ public class AboutFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        aboutViewModel = new ViewModelProvider(this).get(AboutViewModel.class);
        View view = inflater.inflate(R.layout.fragment_about, container, false);
        textView = view.findViewById(R.id.about_text);
        return view;
     }
+
+
 }
