eb435faee19eaf5bc7b807bfc8aeb6526ef5a84d
Pearl Molefe <PMolefe@bec.co.bw>
2022-01-17 09:26:37 +0200

    disable firestore offline caching

--- app/src/main/java/com/example/spotted/services/FirebaseService.java
+++ app/src/main/java/com/example/spotted/services/FirebaseService.java
@@ -1,6 +1,7 @@
 package com.example.spotted.services;
 
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
 
 public class FirebaseService {
     private static FirebaseFirestore FIRESTORE;
@@ -8,6 +9,12 @@ public class FirebaseService {
     public static FirebaseFirestore getFirestore() {
         if (FIRESTORE == null) {
             FIRESTORE = FirebaseFirestore.getInstance();
+
+            //disable offline caching
+            FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                    .setPersistenceEnabled(false)
+                    .build();
+            FIRESTORE.setFirestoreSettings(settings);
         }
 
         return FIRESTORE;
